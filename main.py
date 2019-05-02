23f7c5227b0d1f68f4815a2ba02344fae7174810
laboratorios <laboratorios@Laboratorios>
2019-05-02 20:49:33 -0300

    aula de grafos

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Iniciando em python");
+
+grafo = {"a":["ac", "ca"],
+"b":["cb", "bc", "be", "eb"],
+"c":["ce", "ec", "cd", "dc", "ca", "bc", "cb"],
+"d":["cd", "dc"],
+"e":["eb", "be", "ce", "ec"],
+"f":[]}
+
+grafo["j"] = ["ej", "dj"]
+
+print(grafo)
